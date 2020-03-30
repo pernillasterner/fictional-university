1c5dfa16a2c58902ff334c4e039be6c4a7a7b8a9
Pernilla Sterner <pernilla@stok.se>
2020-03-30 19:57:12 +0200

    Add blog listing page

--- fictional-university/page.php
+++ fictional-university/page.php
@@ -17,10 +17,11 @@
 
       <?php $theParent = wp_get_post_parent_id(get_the_ID()); ?>
         
+
             
       <?php if( $theParent ) { ?>
               <div class="metabox metabox--position-up metabox--with-home-link">
-                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink( $theParent ); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title( $theParent ); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink( $theParent ); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title( $theParent ); ?></a> <span class="metabox__main">Posted by: <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
               </div>
       <?php } ?>
 
